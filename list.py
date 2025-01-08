c4c74c5953592055f74b1d261be51bfe64fb2f5b
Shivathmika Vuppugandla <shivathmikavuppugandla@gmail.com>
2025-01-08 09:38:05 +0530

    assign2

---
+++ list.py
@@ -0,0 +1,7 @@
+#Create a List, tuple and Dictionary with 5 elements in it and how to access few elements based on the index. Try  with different examples 
+a=[1,2,3,4,5]
+b=(1,2,3,4,5)
+c={1:"a",2:"b",3:"c",4:"d",5:"e"}
+print(a[2])
+print(b[1])
+print(c[3])
